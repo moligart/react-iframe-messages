8ee7ba3164a00d2cb2f585c0920c2aad98ad8f6e
Marc Gartner <mogartner@gmail.com>
2018-06-26 20:17:39 -0400

    message listener and ready signal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor() {
+        super();
+
+        this.init = this.init.bind(this);
+        this.handlePostMessage = this.handlePostMessage.bind(this);
+    }
+
+    componentDidMount() {
+        this.init();
+    }
+
+    init = () => {
+        window.addEventListener('message', this.handlePostMessage);
+        window.parent.postMessage('ready', '*');
+    }
+
+    handlePostMessage = (event) => {
+        console.log(event.data);
+    }
+
     render() {
         return (
             <div className="App">
